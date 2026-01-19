1ba1880534ae168af3c1ae629de0150d10e3844e
Andrey Kanaykin <andrey.kanaykin@onetwotrip.com>
2026-01-19 21:03:58 +0300

    docker pt.2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,15 @@
 set -e
 
 echo "Waiting for Postgres"
-until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"; do
+until PGPASSWORD="$DB_PASS" pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"; do
   sleep 1
 done
 
 echo "running migrations..."
 npm run migration:run:prod
 
-echo "Seeding data..."
-npm run seed:prod
+#echo "Seeding data..."
+#npm run seed:prod
 
 echo "Starting app..."
 exec "$@"
